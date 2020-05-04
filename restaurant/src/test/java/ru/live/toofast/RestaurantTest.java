7edc9501189919574496ff482708cfa50149d0b3
nemoedelo <elenakulagina.mail@gmail.com>
2020-05-04 15:24:06 +0300

    Added a test

---
+++ restaurant/src/test/java/ru/live/toofast/RestaurantTest.java
@@ -0,0 +1,13 @@
+package ru.live.toofast;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RestaurantTest {
+
+    @Test
+    public void testSomething() throws InterruptedException {
+        Thread.sleep(3000);
+        Assert.assertTrue(true);
+    }
+}
